af8684732da591ad230b73dc1797cdb37bb8755a
Kody Broussard <kzb801@gmail.com>
2018-10-07 17:07:05 -0500

    Creation of Git Repo for Project 6
    Build a Content Scraper for FSJS Project number 6

---
+++ scraper.js
@@ -0,0 +1,17 @@
+//Project 6 Build a content Scraper
+//Kody Broussard
+//10/4/2018
+//Using NPM, Node.js, vanilla js
+
+//scraper.js is the Command Line Application for the content scraper
+//Create a package.json
+//Program to check for folder called data
+  //Create a folder called data if it does not exist
+
+//Choose third party npm packages
+  //Scraping packaage Cheerio to create my own, or off the shelf
+  //CVS creation package
+  
+
+
+console.log('Hello');
